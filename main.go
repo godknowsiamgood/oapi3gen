fa53ff41c361dedd9dd27789b0dfc89bb3cfe340
naydenishev <godknowsiamgood@gmail.com>
2021-10-19 19:35:44 +0300

    init

---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"api/oapi3"
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/goccy/go-yaml"
+	"github.com/iancoleman/strcase"
+	"go/format"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"text/template"
+)
+
+func PathToMethodName(path string, method string) string {
+	b := strings.Builder{}
+
+	switch method {
+	case "get":
+		b.WriteString("Get")
+	case "post":
+		b.WriteString("Post")
+	case "delete":
+		b.WriteString("Delete")
+	case "update":
+		b.WriteString("Update")
+	}
+
+	r, _ := regexp.Compile("(\\w+)")
+
+	parts := r.FindAllString(path, 10)
+	for _, p := range parts {
+		b.WriteString(strings.Title(p))
+	}
+
+	return b.String()
+}
+
+func toColumnParametersPath(path string) string {
+	path = strings.ReplaceAll(path, "{", ":")
+	path = strings.ReplaceAll(path, "}", "")
+	return path
+}
+
+func templateMap(values ...interface{}) (map[string]interface{}, error) {
+	if len(values)%2 != 0 {
+		return nil, errors.New("invalid dict call")
+	}
+	dict := make(map[string]interface{}, len(values)/2)
+	for i := 0; i < len(values); i += 2 {
+		key, ok := values[i].(string)
+		if !ok {
+			return nil, errors.New("dict keys must be strings")
+		}
+		dict[key] = values[i+1]
+	}
+	return dict, nil
+}
+
+func log(format string, vars... interface{}) {
+	_, _ = fmt.Fprintf(os.Stderr, format + "\n", vars...)
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		log("yml spec file should be provided")
+		return
+	}
+	specFileName := os.Args[1]
+
+	var genFileOutput string
+	if len(os.Args) == 3 {
+		genFileOutput = os.Args[2]
+	} else {
+		name := strings.TrimSuffix(specFileName, filepath.Ext(specFileName))
+		genFileOutput = name + ".go"
+	}
+
+	doc, err := openapi3.NewLoader().LoadFromFile(specFileName)
+	if err != nil {
+		log("schema parsing failed: %v", err)
+		return
+	}
+
+	if err := doc.Validate(context.TODO()); err != nil {
+		log("schema validation failed: %v", err)
+		return
+	}
+
+	s := oapi3.Spec{}
+
+	content, _ := ioutil.ReadFile("./v1.yml")
+	if err := yaml.Unmarshal(content, &s); err != nil {
+		log("schema parsing failed: %v", err)
+		return
+	}
+
+	t, _ := template.New("echo.tmpl").Funcs(template.FuncMap{
+		"pathToMethodName":       PathToMethodName,
+		"toCamel":                strcase.ToCamel,
+		"toUpper":                strings.ToUpper,
+		"toColumnParametersPath": toColumnParametersPath,
+		"dict":                   templateMap,
+	}).ParseFiles("echo.tmpl")
+
+	sb := new(bytes.Buffer)
+
+	if err := t.Execute(sb, s); err != nil {
+		log("code generation failed: %v", err)
+		return
+	}
+
+	formattedSource, err := format.Source(sb.Bytes())
+	if err != nil {
+		log("code formatting failed: %v", err)
+		return
+	}
+
+	genDirOutput := filepath.Dir(genFileOutput)
+	if err := os.MkdirAll(genDirOutput, 0750); err != nil {
+		log("saving generated code failed: %v", err)
+	}
+	if err := ioutil.WriteFile(genFileOutput, formattedSource, 0755); err != nil {
+		log("saving generated code failed: %v", err)
+		return
+	}
+}
---
+++ oapi3/objects.go
@@ -0,0 +1,277 @@
+package oapi3
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type Ref string
+
+func (r Ref) NotSet() bool {
+	return r == ""
+}
+
+func (r Ref) GetName() (string, string) {
+	reg, _ := regexp.Compile("#/components/(schemas|responses)/(.+)")
+	res := reg.FindAllStringSubmatch(string(r), 2)
+	return res[0][1], res[0][2]
+}
+
+func (r Ref) GetTypeName() string {
+	typ, name := r.GetName()
+
+	switch typ {
+	case "schemas":
+		return name
+	case "responses":
+		return name + "Response"
+	}
+
+	return "Unknown"
+}
+
+type Type string
+
+func (t Type) IsObject() bool {
+	return t == "object"
+}
+
+func (t Type) GetTypeName() string {
+	switch t {
+	case "string":
+		return "string"
+	case "number":
+		return "float64"
+	case "integer":
+		return "int64"
+	case "boolean":
+		return "bool"
+	}
+	return "<unknown_type>"
+}
+
+type Info struct {
+	Title       string `yaml:"title"`
+	Description string `yaml:"description"`
+	Version     string `yaml:"version"`
+}
+
+type Parameter struct {
+	In          string `yaml:"in"`
+	Name        string `yaml:"name"`
+	Required    bool   `yaml:"required"`
+	Description string `yaml:"description"`
+	Schema      Schema `yaml:"schema"`
+}
+
+func (p Parameter) IsRequired() bool {
+	return p.Required || p.In == "path"
+}
+
+type Schema struct {
+	AllOf []Schema `yaml:"allOf"`
+
+	Ref           Ref               `yaml:"$ref"`
+	Description   string            `yaml:"description"`
+	Type          Type              `yaml:"type"`
+	Minimum       *int              `yaml:"minimum"`
+	Maximum       *int              `yaml:"maximum"`
+	MinimumLength *int              `yaml:"minLength"`
+	MaximumLength *int              `yaml:"maxLength"`
+	Default       *string           `yaml:"default"`
+	Required      []string          `yaml:"required"`
+	Enum          []string          `yaml:"enum"`
+	Items         *Schema           `yaml:"items"`
+	Properties    map[string]Schema `yaml:"properties"`
+}
+func (s Schema) GetValidationTags(isRequired bool) string {
+	var tags []string
+
+	if isRequired {
+		tags = append(tags, "required")
+	}
+	if len(s.Enum) > 0 {
+		tags = append(tags, "oneof=" + strings.Join(s.Enum, " "))
+	}
+	if s.Minimum != nil {
+		tags = append(tags, "min=" + strconv.Itoa(*s.Minimum))
+	}
+	if s.Maximum != nil {
+		tags = append(tags, "max=" + strconv.Itoa(*s.Maximum))
+	}
+	if s.MinimumLength != nil {
+		tags = append(tags, "min=" + strconv.Itoa(*s.MinimumLength))
+	}
+	if s.MaximumLength != nil {
+		tags = append(tags, "max=" + strconv.Itoa(*s.MaximumLength))
+	}
+	return strings.Join(tags, ",")
+}
+func (s Schema) ExpandAllOf() {
+	if len(s.AllOf) == 0 {
+		return
+	}
+
+	s.Type = "object"
+
+	if s.Properties == nil {
+		s.Properties = make(map[string]Schema)
+	}
+
+	for _, e := range s.AllOf {
+		for k, v := range e.Properties {
+			s.Properties[k] = v
+		}
+	}
+}
+
+func (s Schema) GetSerializedEnums(asStrings bool) string {
+	if len(s.Enum) > 0 {
+		b := strings.Builder{}
+		if asStrings {
+			b.WriteString("[]string{")
+		} else {
+			b.WriteString("[]int64{")
+		}
+
+		for _, e := range s.Enum {
+			if asStrings {
+				b.WriteString("\"")
+			}
+			b.WriteString(e)
+			if asStrings {
+				b.WriteString("\"")
+			}
+			b.WriteString(",")
+		}
+		b.WriteString("}")
+		return b.String()
+	} else {
+		return "nil"
+	}
+}
+
+func (s Schema) IsFieldRequired(fieldName string) bool {
+	for _, r := range s.Required {
+		if fieldName == r {
+			return true
+		}
+	}
+	return false
+}
+
+func (s Schema) IsObject() bool {
+	return s.Type == "object"
+}
+func (s Schema) IsSet() bool {
+	return !s.Ref.NotSet() || len(s.AllOf) > 0 || s.Type != ""
+}
+func (s Schema) GetDefault() string {
+	if s.Default == nil {
+		return "nil"
+	} else {
+		if s.IsNumeric() {
+			return "intPointer(" + (*s.Default) + ")"
+		} else {
+			return "stringPointer(\"" + (*s.Default) + "\")"
+		}
+	}
+}
+func (s Schema) GetMinimum() string {
+	if !s.IsNumeric() || s.Minimum == nil {
+		return "nil"
+	} else {
+		return "intPointer(" + strconv.Itoa(*s.Minimum) + ")"
+	}
+}
+func (s Schema) GetMaximum() string {
+	if !s.IsNumeric() || s.Maximum == nil {
+		return "nil"
+	} else {
+		return "intPointer(" + strconv.Itoa(*s.Maximum) + ")"
+	}
+}
+func (s Schema) GetMinimumLength() string {
+	if s.IsNumeric() || s.MinimumLength == nil {
+		return "nil"
+	} else {
+		return "intPointer(" + strconv.Itoa(*s.MinimumLength) + ")"
+	}
+}
+func (s Schema) GetMaximumLength() string {
+	if s.IsNumeric() || s.MaximumLength == nil {
+		return "nil"
+	} else {
+		return "intPointer(" + strconv.Itoa(*s.MaximumLength) + ")"
+	}
+}
+func (s Schema) IsNumeric() bool {
+	return s.Type == "number" || s.Type == "integer"
+}
+func (s Schema) GetGoType() string {
+	if s.Ref != "" {
+		return s.Ref.GetTypeName()
+	}
+
+	b := strings.Builder{}
+
+	if s.Type == "array" {
+		b.WriteString("[]")
+		if s.Items.Ref != "" {
+			b.WriteString(s.Items.Ref.GetTypeName())
+		} else {
+			b.WriteString(s.Items.Type.GetTypeName())
+		}
+	} else {
+		b.WriteString(s.Type.GetTypeName())
+	}
+
+	return b.String()
+}
+
+type Response struct {
+	Ref         Ref    `yaml:"$ref"`
+	Description string `yaml:"description"`
+	Content     struct {
+		JSON struct {
+			Schema Schema `yaml:"schema"`
+		} `yaml:"application/json"`
+	} `yaml:"content"`
+}
+
+type Operation struct {
+	Parameters  []Parameter         `yaml:"parameters"`
+	Summary     string              `yaml:"summary"`
+	Description string              `yaml:"description"`
+	Responses   map[string]Response `yaml:"responses"`
+}
+
+func (op Operation) GetResponses() map[string]Response {
+	responses := make(map[string]Response)
+	for status, response := range op.Responses {
+		if _, err := strconv.Atoi(status); err != nil {
+			continue
+		}
+		if response.Ref.NotSet() && !response.Content.JSON.Schema.IsSet() {
+			continue
+		}
+		responses[status] = response
+	}
+	return responses
+}
+
+type Components struct {
+	Schemas   map[string]Schema   `yaml:"schemas"`
+	Responses map[string]Response `yaml:"responses"`
+}
+
+type PathOperation map[string]Operation
+
+type Spec struct {
+	Swagger    string                   `yaml:"swagger"`
+	Info       Info                     `yaml:"info"`
+	BasePath   string                   `yaml:"basePath"`
+	Paths      map[string]PathOperation `yaml:"paths"`
+	Components Components               `yaml:"components"`
+}
